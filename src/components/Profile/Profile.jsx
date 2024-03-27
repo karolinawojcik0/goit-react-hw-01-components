a8942eeda0ea19fa8fc665c74b3f75bf44514e94
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-03-27 10:23:55 +0100

    .

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,36 @@
 import PropTypes from "prop-types";
-import user from 'components/data/user.json'
-import css from './Profile.module.css'
+import user from 'data/user.json';
 
-<div className="profile-card">
-  <div className="description">
-    <img
-      src={user.avatar}
-      alt="User avatar"
-      className="avatar"
-    />
+export const Profile = () => {
+  return (
+    <div className="profile-card">
+      <div className="description">
+        <img
+          src={user.avatar}
+          alt="User avatar"
+          className="avatar"
+        />
         <p className="name">{user.username}</p>
         <p className="tag">{user.tag}</p>
         <p className="location">{user.location}</p>
-  </div>
+      </div>
 
-  <ul className="stats">
-    <li>
-      <span className="label">Followers</span>
-            <span className="quantity">{user.stats.followers}</span>
-    </li>
-    <li>
-      <span className="label">Views</span>
-            <span className="quantity">{user.stats.views}</span>
-    </li>
-    <li>
-      <span className="label">Likes</span>
-            <span className="quantity">{user.stats.likes}</span>
-    </li>
-  </ul>
-</div>
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{user.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{user.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+
