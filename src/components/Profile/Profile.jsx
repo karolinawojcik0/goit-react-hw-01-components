18a576e03a5e1dea060b38af86383d2237e90c24
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-04-01 12:42:25 +0200

    getRandomHexColor

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,35 @@
-import { Location, ProfileCard, ProfileImage, ProfileTag, Username } from './Profile.css';
+import {
+  Location,
+  ProfileCard,
+  ProfileImage,
+  ProfileTag,
+  Username,
+} from './Profile.css';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <>
-    <ProfileCard>
-    <ProfileImage
-          src={avatar}
-          alt="User avatar"
-        />
+      <ProfileCard>
+        <ProfileImage src={avatar} alt="User avatar" />
         <Username>{username}</Username>
         <ProfileTag>{tag}</ProfileTag>
         <Location>{location}</Location>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    
-    </ProfileCard >
-  </>
+        <ul className="stats">
+          <li>
+            <span className="label">Followers</span>
+            <span className="quantity">{stats.followers}</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className="quantity">{stats.views}</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className="quantity">{stats.likes}</span>
+          </li>
+        </ul>
+      </ProfileCard>
+    </>
   );
 };
-
-
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,10 @@
+import React from 'react';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 export const Statistics = ({ title, stats }) => {
-  // console.log(stats);
   return (
     <>
       <section className="statistics">
@@ -7,7 +12,11 @@ export const Statistics = ({ title, stats }) => {
 
         <ul className="stat-list">
           {stats.map(stat => (
-            <li key={stat.id} className="item">
+            <li
+              key={stat.id}
+              className="item"
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
               <span className="label">{stat.label}</span>
               <span className="percentage">{stat.percentage}</span>
             </li>
